d28d270fa9fc7fa87eb05a2cba0ba28ee5a57e7f
nakamurj23 <nakamurj23@up.edu>
2021-03-08 13:04:14 -1000

    added OnClick

--- app/src/main/java/com/example/mancalagamestate/MainActivity.java
+++ app/src/main/java/com/example/mancalagamestate/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.mancalagamestate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.lang.reflect.Constructor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        Button runTest = findViewById(R.id.runTest);
+    }
+
+    public void onClick(View button){
+        MancalaGameState firstInstance = new MancalaGameState();
+        MancalaGameState secondInstance = new MancalaGameState(firstInstance);
+
+        TextView editText = findViewById(R.id.editText);
+
+        // Legal move
+        editText.setText(" ");
+        firstInstance.selectPit(1, 3);
+        editText.setText("Player one has selected pit 3");
+
     }
 }
